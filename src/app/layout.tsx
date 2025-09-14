a30243dceecedf3c70b0d0167ccdcfd86ede8c99
pepoo202020 <bvbbvi68@gmail.com>
2025-09-14 12:33:46 +0300

    installed dark and light theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import {
   Roboto,
 } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/providers/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -67,7 +68,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${cairo.variable} ${kufam.variable} ${cinzel.variable} ${lora.variable} ${roboto.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
